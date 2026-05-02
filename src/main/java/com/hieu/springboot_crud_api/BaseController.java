028a3abc89395ddd11a01fb493cd118a1931df28
eaves0118 <tranhieudz2024@gmail.com>
2026-05-02 16:39:39 +0700

    first commit

---
+++ src/main/java/com/hieu/springboot_crud_api/BaseController.java
@@ -0,0 +1,15 @@
+package com.hieu.springboot_crud_api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class BaseController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello world";
+    }
+}
---
+++ src/test/java/com/hieu/springboot_crud_api/SpringbootCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hieu.springboot_crud_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
